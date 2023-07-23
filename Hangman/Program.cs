9939549efe8b29554c2de4619a9441ead87dc2f8
BenDev0803 <96314707+BenDev0803@users.noreply.github.com>
2023-07-23 22:11:47 +0200

    got a random entry from a list

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -4,9 +4,12 @@ namespace Hangman // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        public static readonly Random randomChoice = new Random();
         static void Main(string[] args)
         {
-            List<String> possibleWords = new List<String>() { "House", "Room", "Kitchen", "Toilet"};
+            List<String> possibleWords = new List<String>() { "House", "Room", "Kitchen", "Toilet" };
+            int computerChoice = randomChoice.Next(0, possibleWords.Count);
+            Console.WriteLine(possibleWords[computerChoice]);
         }
     }
 }
