00e4e0b5a2f34222aae61a1b9ffedd96e892351b
BenDev0803 <96314707+BenDev0803@users.noreply.github.com>
2023-07-25 02:44:16 +0200

    Written a block that finds out if a character is part of a string

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -9,7 +9,31 @@ namespace Hangman // Note: actual namespace depends on the project name.
         {
             List<String> possibleWords = new List<String>() { "House", "Room", "Kitchen", "Toilet" };
             int computerChoice = randomChoice.Next(0, possibleWords.Count);
-            Console.WriteLine(possibleWords[computerChoice]);
+            // Console.WriteLine(possibleWords[computerChoice]);
+            Console.WriteLine($"Hangman game! type a letter and see if it matches with a word letter!");
+            string userOutput = Console.ReadLine();
+
+            for (int i = 0; i < possibleWords[computerChoice].Length; i++)
+            {
+                if (possibleWords[computerChoice].Contains(userOutput))
+                {
+                    Console.WriteLine("true!");
+                }
+                else
+                {
+                    Console.WriteLine("try again");
+                }
+            }
+            
+            /*if(userOutput == possibleWords[computerChoice])
+            {
+                Console.WriteLine("true!");
+            }
+            else
+            {
+                Console.WriteLine("try again");
+            } 
+            */
         }
     }
 }
