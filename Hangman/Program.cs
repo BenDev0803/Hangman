9fc86750a41535ae0a048cc34da1dd8b209f0357
BenDev0803 <96314707+BenDev0803@users.noreply.github.com>
2023-07-26 21:06:08 +0200

    added new list for right characters, added loop

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -8,20 +8,34 @@ namespace Hangman // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             List<String> possibleWords = new List<String>() { "House", "Room", "Kitchen", "Toilet" };
+            List<Char> guessedWord = new List<Char>();
             int computerChoice = randomChoice.Next(0, possibleWords.Count);
             string randomWord = possibleWords[computerChoice];
             Console.WriteLine($"Hangman game! type a letter and see if it matches with a word letter!");
             ConsoleKeyInfo userOutput = Console.ReadKey();
             Char userLetter = userOutput.KeyChar;
+            Char line = '-';
+
+
+            for (int i= 0; i < randomWord.Length; i++)
+            {
+                guessedWord.Add(line);
+            }
+
             for (int i = 0; i < randomWord.Length; i++)
-            { 
-                if(userLetter == randomWord[i])
+            {
+                if (userLetter == randomWord[i])
                 {
-                    Console.WriteLine("true!" + randomWord);
+                    
+                    guessedWord.ForEach(Console.WriteLine);
+                    Console.ReadKey();
+                    Console.WriteLine(randomWord);
                 }
                 else
                 {
-                    Console.WriteLine("false");
+                    Console.WriteLine(line);
+                    Console.ReadKey();
+                    Console.WriteLine(randomWord);
                 }
             }
         }
