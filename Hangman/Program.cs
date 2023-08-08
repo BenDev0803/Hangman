3c243dd959fc47c41324611efadab02ca12554c3
BenDev0803 <96314707+BenDev0803@users.noreply.github.com>
2023-08-08 10:27:03 +0200

    implemented outer loop for more attempts

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -13,49 +13,35 @@ namespace Hangman // Note: actual namespace depends on the project name.
             string randomWord = words[computerChoice];
             Console.WriteLine($"Hangman game! type a letter and see if it matches with a word letter!");
             Char line = '-';
-
-
+            List<char> wordList = new List<char>(); 
             for (int i = 0; i < randomWord.Length; i++)
             {
                 userWord.Add(line);
             }
             userWord.ForEach(Console.Write); //TODO: maybe write with a for loop / foreach loop
-
             int attempts = randomWord.Length + 5;
             //foreach (var item in collection)
             //{
-
-            //}
-
-                
-
+            //}  
             for (int j = attempts; j >= 0 ; j--)
             {
                 ConsoleKeyInfo userChoiceKeyInfo = Console.ReadKey();
                 Char userChoiceChar = userChoiceKeyInfo.KeyChar;
                 for (int i = 0; i < randomWord.Length; i++)
                 {
-                    
                     if (userChoiceChar == randomWord[i])
                     {
                         userWord[i] = randomWord[i];
-                        
+                        wordList.Add(userWord[i]);
                     }
                     else
                     {
                         userWord[i] = line;
                     }
-
                     //Console.WriteLine(userWord);
-                    
                 }
-                userWord.ForEach(Console.Write);
+                wordList.ForEach(Console.Write);
             }
-            
-
-            
-            
-
            // Console.ReadKey();
         }
     }
