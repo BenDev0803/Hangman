b7b9dfa3bbce09bacbf7b68ce60453483db720ce
BenDev0803 <96314707+BenDev0803@users.noreply.github.com>
2023-08-08 06:29:19 +0200

    implemented outer loop for more guesses

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -7,39 +7,56 @@ namespace Hangman // Note: actual namespace depends on the project name.
         public static readonly Random randomChoice = new Random();
         static void Main(string[] args)
         {
-            List<String> possibleWords = new List<String>() { "House", "Room", "Kitchen", "Toilet" };
-            List<Char> guessedWord = new List<Char>();
-            int computerChoice = randomChoice.Next(0, possibleWords.Count);
-            string randomWord = possibleWords[computerChoice];
+            List<String> words = new List<String>() { "House" };// , "Room", "Kitchen", "Toilet" };
+            List<Char> userWord = new List<Char>();
+            int computerChoice = randomChoice.Next(0, words.Count);
+            string randomWord = words[computerChoice];
             Console.WriteLine($"Hangman game! type a letter and see if it matches with a word letter!");
             Char line = '-';
 
 
-            for (int i= 0; i < randomWord.Length; i++)
-            {
-                guessedWord.Add(line);
-                
-            }
-            guessedWord.ForEach(Console.Write);
-
-            ConsoleKeyInfo userOutput = Console.ReadKey();
-            Char userLetter = userOutput.KeyChar;
-
             for (int i = 0; i < randomWord.Length; i++)
             {
-                if (userLetter == randomWord[i])
-                {
-                    guessedWord[i] = randomWord[i];
-                    guessedWord.ForEach(Console.Write);
-                }
-                else
-                {
-                    guessedWord[i] = line;
-                    guessedWord.ForEach(Console.Write);
-                }
+                userWord.Add(line);
             }
-            guessedWord.ForEach(Console.Write);
+            userWord.ForEach(Console.Write); //TODO: maybe write with a for loop / foreach loop
 
+            int attempts = randomWord.Length + 5;
+            //foreach (var item in collection)
+            //{
+
+            //}
+
+                
+
+            for (int j = attempts; j >= 0 ; j--)
+            {
+                ConsoleKeyInfo userChoiceKeyInfo = Console.ReadKey();
+                Char userChoiceChar = userChoiceKeyInfo.KeyChar;
+                for (int i = 0; i < randomWord.Length; i++)
+                {
+                    
+                    if (userChoiceChar == randomWord[i])
+                    {
+                        userWord[i] = randomWord[i];
+                        
+                    }
+                    else
+                    {
+                        userWord[i] = line;
+                    }
+
+                    //Console.WriteLine(userWord);
+                    
+                }
+                userWord.ForEach(Console.Write);
+            }
+            
+
+            
+            
+
+           // Console.ReadKey();
         }
     }
 }
