429e4cb542ef2efa9e11f6c5a915a3b811703212
BenDev0803 <96314707+BenDev0803@users.noreply.github.com>
2023-08-15 13:08:34 +0200

    resolved issue number #5 and issue number #2

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -14,14 +14,10 @@ namespace Hangman // Note: actual namespace depends on the project name.
             Console.WriteLine($"Hangman game! type a character and see if it matches with a word letter!");
             Char line = '-';
 
-            for (int i = 0; i < randomWord.Length; i++)
-            {
-                
-                userWord.Add(line);
-            
-            }
+            for (int i = 0; i < randomWord.Length; i++){userWord.Add(line);}
 
-            userWord.ForEach(Console.Write); //TODO: maybe write with a for loop / foreach loop
+            for( int i = 0; i < userWord.Count; i++){Console.Write(userWord[i]);}
+
             const int additionalAttempts = 5;
             int attempts = randomWord.Length + additionalAttempts;
             
@@ -36,30 +32,17 @@ namespace Hangman // Note: actual namespace depends on the project name.
                 
                 Console.Clear();
 
-                if (randomWord == combinedString)
-                {
-                    Console.WriteLine(" you won!");
-                    break;
-                }
+                if (randomWord == combinedString){Console.WriteLine(" you won!");break;}
 
-                for (int i = 0; i < randomWord.Length; i++)
-                {
-                    
-                    if (userChoiceChar == randomWord[i])
-                    {
-                        
-                        userWord[i] = randomWord[i];
-                        
-                    }
-                }
+                for (int i = 0; i < randomWord.Length; i++){if (userChoiceChar == randomWord[i]){userWord[i] = randomWord[i];}}
+                
                 userWord.ForEach(Console.Write);
+                
                 Console.Write($" remaining attempts {j}");
             }
             
-            if (userWord.Contains('-'))
-            {
-                Console.WriteLine(" you lost!");
-            }
+            if (userWord.Contains('-')){Console.WriteLine(" you lost!");}
+
         }
     }
 }
